aa16a18a73335fb30b6ee762a1670a67f7d49b3b
You-saku <53119949+You-saku@users.noreply.github.com>
2023-10-14 22:08:03 +0900

    add: zodの導入 (#3)

--- src/components/ChatPage.tsx
+++ src/components/ChatPage.tsx
@@ -1,22 +1,41 @@
-import React, { useState } from 'react'
 import {
     Box,
     Button,
     Center,
     FormControl,
-    FormLabel,
     FormErrorMessage,
+    FormLabel,
     FormHelperText, 
     Input,
     Text,
     VStack,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { ZodError, z } from 'zod';
+
 
 export default function ChatPage() {
     const [input, setInput] = useState('')
+    const [validated, setValidated] = useState('')
+    const [isValid, setIsValid] = useState(false)
+    const [errorMessage, setErrMessage] = useState<string|null>(null)
     const [response, setResponse] = useState('')
 
+    const ChatSchema = z.string().min(1, { message: 'Please input more than 1 character.' });
+
     const chat = async () => {
+        setIsValid(false)
+
+        try {
+            const validated = ChatSchema.parse(input);
+            setValidated(validated)
+        } catch (err: any) {
+            if (err instanceof ZodError) {
+                setErrMessage(err.format()._errors[0])
+                setIsValid(true)
+                return
+            }
+        }
 
         const res = await fetch(process.env.REACT_APP_CLOUDFLAWE_WORKERS_AI_API || "", {
             method: 'POST',
@@ -24,7 +43,7 @@ export default function ChatPage() {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                    prompt: input
+                    prompt: validated
             })
         }).then(res => res.json())
         .then(data => data.response)
@@ -39,9 +58,10 @@ export default function ChatPage() {
         <Center py={6}>
             <VStack spacing={6}>
             <Text fontSize='3xl'>Chat with Cloudflare Workers AI</Text>
-            <FormControl>
+            <FormControl isInvalid={isValid}>
                 <FormLabel>Input</FormLabel>
                 <Input type='text' value={input} onChange={(e) => setInput(e.target.value)}/>
+                {errorMessage && <FormErrorMessage>{errorMessage}</FormErrorMessage>}
                 <FormHelperText>Please type what you want to ask(Only English)</FormHelperText>
             </FormControl>
             <Button colorScheme='orange' size='md' type="submit" onClick={chat}>Submit</Button>
