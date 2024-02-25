453df2521d7495b969c5ed468148bc38de3a8788
You-saku <yousaku.dev19@gmail.com>
2024-02-25 22:08:22 +0900

    fix: chatページ修正

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -4,7 +4,6 @@ import {
     Center,
     FormControl,
     FormErrorMessage,
-    FormLabel,
     FormHelperText,
     Flex,
     Input,
@@ -16,26 +15,24 @@ import { ChatSchema } from '../schemas/chatSchema';
 import { cloudflareWorkersAI } from '../hooks/cloudflare-workers-ai';
 
 export default function Chat() {
-    const [input, setInput] = useState('')
-    const [validated, setValidated] = useState('')
-    const [isValid, setIsValid] = useState(false)
+    const [input, setInput] = useState<string>('')
+    const [isInvalid, setIsInvalid] = useState<boolean>(false)
     const [validateErrorMessage, setValidateErrMessage] = useState<string|null>(null)
     const [response, setResponse] = useState('')
 
     const chat = async () => {
-        setIsValid(false)
-
+        let validatedInput = "";
         try {
-            const validated = ChatSchema.parse(input);
-            setValidated(validated)
+            validatedInput = ChatSchema.parse(input);
+            setIsInvalid(false);
         } catch (err: any) {
             if (err instanceof ZodError) {
-                setValidateErrMessage(err.format()._errors[0])
-                setIsValid(true)
+                setValidateErrMessage(err.format()._errors[0]);
+                setIsInvalid(true);
                 return
             }
         }
-        const res = await cloudflareWorkersAI(validated);
+        const res = await cloudflareWorkersAI(validatedInput);
 
         setResponse(res)
         setInput('')
@@ -44,17 +41,23 @@ export default function Chat() {
     return (
       <div>
         <Center>
-            <Flex direction='column' align='center' justify='center'>
-                <Text fontSize='3xl'>Chat with Cloudflare Workers AI</Text>
-                <FormControl isInvalid={isValid}>
-                    <FormLabel>Input</FormLabel>
-                    <Input type='text' value={input} onChange={(e) => setInput(e.target.value)}/>
-                    {validateErrorMessage && <FormErrorMessage>{validateErrorMessage}</FormErrorMessage>}
-                    <FormHelperText>Please type what you want to ask(Only English)</FormHelperText>
-                </FormControl>
-                <Button colorScheme='orange' size='md' type="submit" m={'1rem'} onClick={chat}>Submit</Button>
-                <Text>Output</Text>
-                {response && <Box maxW={'24rem'} p={'0.5rem'} border={'1px'}>{response}</Box>}
+            <Flex direction='column'>
+                <Box>
+                    <Text fontSize='3xl'>Chat with Cloudflare Workers AI</Text>
+                    <Text fontWeight="medium">Your Question</ Text>
+                    <FormControl isInvalid={isInvalid} marginBottom={'1rem'}>
+                        <Flex direction={'row'}>
+                            <Input type='text' value={input} onChange={(e) => setInput(e.target.value)}/>
+                            <Button colorScheme='orange' marginLeft={'0.5rem'} type="submit" onClick={chat}>Submit</Button>
+                        </Flex>
+                        {validateErrorMessage && <FormErrorMessage>{validateErrorMessage}</FormErrorMessage>}
+                        <FormHelperText>Please type what you want to ask(Only English)</FormHelperText>
+                    </FormControl>
+                </Box>
+                <Box>
+                    <Text fontWeight="medium">Workers AI Answer</ Text>
+                    {response && <Box maxW={'27rem'} p={'0.5rem'} border={'1px'}>{response}</Box>}
+                </Box>
             </Flex>
         </Center>
       </div>
