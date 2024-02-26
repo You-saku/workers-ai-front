8d332c17618bf826acb083e8cf8b949cdb6ff3ab
You-saku <53119949+You-saku@users.noreply.github.com>
2024-02-26 23:14:20 +0900

    react-hook-formを導入 (#11)
    * add: react-hook-form導入
    * fix: フォームとバリデーションを修正
    * fix: onChangeの記述をhook-formにならう

--- src/schemas/chatSchema.ts
+++ src/schemas/chatSchema.ts
@@ -1,3 +1,5 @@
 import { z } from 'zod';
 
-export const ChatSchema = z.string().min(1, { message: 'Please input more than 1 character.' });
+export const ChatSchema = z.object({
+        'question': z.string().min(1, { message: 'Please input more than 1 character.' }),
+    });
