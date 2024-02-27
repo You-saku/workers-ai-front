a966ed83b7fae4e469af8712d24ced5bf2d731b6
You-saku <yousaku.dev19@gmail.com>
2024-02-28 00:28:35 +0900

    test: レスポンスオブジェクトを返してみる

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,3 +1,8 @@
 export async function onRequest(context: any) {
     console.log('onRequest middleware');
+    try {
+        return await context.next();
+    } catch (err: any) {
+        return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+    }
 }
